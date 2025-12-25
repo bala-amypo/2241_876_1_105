01d8a1f30172a10d2e4100992c6590f594f4df22
bala-amypo <bala@amypo.in>
2025-12-25 06:51:18 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
+@EntityScan("com.example.demo.model")
+@EnableJpaRepositories("com.example.demo.repository")
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
