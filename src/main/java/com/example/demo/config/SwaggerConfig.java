738dcb951b88b4445d6a4c904b9dadb87ac70eb2
bala-amypo <bala@amypo.in>
2025-12-25 14:45:37 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,20 @@
 
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.*;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.annotations.*;
+// import io.swagger.v3.oas.annotations.info.Info;
+// import io.swagger.v3.oas.annotations.security.*;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@OpenAPIDefinition(
-        info = @Info(title = "Digital Visitor Management API", version = "1.0"),
-        security = @SecurityRequirement(name = "bearerAuth")
-)
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
-)
-public class SwaggerConfig {}
+// @Configuration
+// @OpenAPIDefinition(
+//         info = @Info(title = "Digital Visitor Management API", version = "1.0"),
+//         security = @SecurityRequirement(name = "bearerAuth")
+// )
+// @SecurityScheme(
+//         name = "bearerAuth",
+//         type = SecuritySchemeType.HTTP,
+//         scheme = "bearer",
+//         bearerFormat = "JWT"
+// )
+// public class SwaggerConfig {}
