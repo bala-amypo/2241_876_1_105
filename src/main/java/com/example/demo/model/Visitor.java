0f359f624db1e53b2f7ae502075094f70aacad4b
bala-amypo <bala@amypo.in>
2025-12-25 13:39:14 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,25 +1,32 @@
-package com.example.demo.model;
-import jakarta.persistence.Entity;
-public class Visitor {
 
-    private Long id;
-    private String fullName;
-    private String email;
-    private String phone;
-    private String idProofNumber;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
 
-    public String getIdProofNumber() { return idProofNumber; }
-    public void setIdProofNumber(String idProofNumber) { this.idProofNumber = idProofNumber; }
-}
+
+// package com.example.demo.model;
+// import jakarta.persistence.Entity;
+// public class Visitor {
+
+//     private Long id;
+//     private String fullName;
+//     private String email;
+//     private String phone;
+//     private String idProofNumber;
+
+//     public Long getId() { return id; }
+//     public void setId(Long id) { this.id = id; }
+
+//     public String getFullName() { return fullName; }
+//     public void setFullName(String fullName) { this.fullName = fullName; }
+
+//     public String getEmail() { return email; }
+//     public void setEmail(String email) { this.email = email; }
+
+//     public String getPhone() { return phone; }
+//     public void setPhone(String phone) { this.phone = phone; }
+
+//     public String getIdProofNumber() { return idProofNumber; }
+//     public void setIdProofNumber(String idProofNumber) { this.idProofNumber = idProofNumber; }
+// }
